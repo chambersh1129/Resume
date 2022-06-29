7456d4911fbcd9b729cd5453d32cb82fe3864eb5
Hunter Chambers <chambersh1129@gmail.com>
2022-06-29 12:04:48 -0400

    removing debug settings

--- config/settings.py
+++ config/settings.py
@@ -81,9 +81,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "config.wsgi.application"
 
-if DEBUG:
-    INSTALLED_APPS.append("django_extensions")
-
 # Security Settings
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)

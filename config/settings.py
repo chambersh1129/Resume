23a566e8299fd72eab3424a6690b848d3c429b6e
Hunter Chambers <chambersh1129@gmail.com>
2022-06-29 10:52:52 -0400

    still playing with deployment settings

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ environ.Env.read_env(BASE_DIR / ".env")
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DJANGO_DEBUG", default=True)
+DEBUG = env.bool("DJANGO_DEBUG", default=False)
 
 ALLOWED_HOSTS = ["chambersh1129-resume.herokuapp.com", "localhost", "127.0.0.1"]
 

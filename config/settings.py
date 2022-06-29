33c8d4aa8b16aaadbe94fefdcbdc6db98dfcfe15
Hunter Chambers <chambersh1129@gmail.com>
2022-06-29 11:23:41 -0400

    codifying the database string

--- config/settings.py
+++ config/settings.py
@@ -104,12 +104,7 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = {"default": env.db_url("DATABASE_URL", default="sqlite:///db.sqlite3")}
 
 
 # Password validation
